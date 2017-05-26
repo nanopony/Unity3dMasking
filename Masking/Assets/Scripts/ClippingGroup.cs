6a83d67cfb9671308617ae981258f9490c1f0afc
SD <sddeath@gmail.com>
2017-05-26 18:22:46 +0300

    Initial commit

---
+++ Masking/Assets/Scripts/ClippingGroup.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClippingGroup : MonoBehaviour {
+	public int ClippingGroupId = 1;
+
+	public void OnValidate() {
+		_updateClippingGroup();
+	}
+
+
+	private void _updateClippingGroup() {
+		var r = GetComponent<Renderer> ();
+		r.material.SetInt ("_ClippingGroup", ClippingGroupId);
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
